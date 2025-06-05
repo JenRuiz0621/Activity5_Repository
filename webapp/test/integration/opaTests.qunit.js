ccbb19d28dff937ec1ef095d508bf6e353d37291
Jeniffer Ruiz <jeny.21ph@gmail.com>
2025-06-05 22:04:36 +0000

    initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/acn/training/activity5/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/acn/training/activity5/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
